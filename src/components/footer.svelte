63bcf2bfb402f4829738ef3c5a1282a70d4250a5
Peter Hamilton <peterhamilton522@Gmail.com>
2024-02-24 00:02:42 +0000

    Styling update

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,30 +1,89 @@
-<!-- Footer.svelte -->
 <script>
-    // You can add script logic here if needed
+    import '@fontsource-variable/exo-2';
+
+    import instagram from '$lib/assets/icons/instagram.svg';
+    import email from '$lib/assets/icons/email.svg';
+    import linkedin from '$lib/assets/icons/linkedin-1.svg';
 </script>
 
 <style>
-    /* Add your CSS styles here */
     .footer {
-      background: #111111; /* Dark background color */
-      color: white;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: relative;
-      padding: 1vh; 
-      margin: 0; 
-      max-width: 100%;
-      border-top: 1px solid rgb(255, 255, 255);
+        background: #111111;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        padding: 1vh; 
+        margin: 0; 
+        max-width: 100%;
+        border-top: 1px solid rgb(255, 255, 255);
     }
 
     .footer p {
         margin: 0;
+        font-family: 'Exo 2 Variable', sans-serif;
+        padding: 1vh;
+        font-size: 18px;
+    }
+
+    .footer p2 {
+        margin: 0;
+        font-family: 'Exo 2 Variable', sans-serif;
+        padding: 1vh;
+        font-size: 16px;
+        font-style: italic;
+
+    }
+
+    .footer-icons {
+        display: flex;
+        justify-content: space-evenly; 
+        align-items: center;
+        width: 50%; 
+        padding: 4vh;
+    }
+
+    .footer-icons a {
+        flex-grow: 1; 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative; 
+        text-decoration: none;
+        
+    }
+
+    .footer-icons img {
+        width: 100%; 
+        max-width: 4vh; 
+        height: auto;
+        margin: 0 2vw;
+        filter: invert(1);
+        transition: filter 0.3s ease
+    }
+
+    .footer-icons img:hover {
+        filter:invert(0);
+    }
+
+    .footer-icons a:not(:last-child)::after {
+        content: "";
+        border-right: 1px solid white;
+        height: 80%;
+        position: absolute;
+        right: 0; 
+        top: 10%; 
     }
 </style>
 
 <div class="footer">
-    <div class="footer-links">
-        <p>&copy; L.A.S.E.R 2023</p>
+    <div class="footer-icons">
+        <a href="https://www.instagram.com/uol_laser/"><img src={instagram} alt="Instagram"></a>
+        <a href="https://www.linkedin.com/company/laser-liverpool/"><img src={linkedin} alt="LinkedIn"></a>
+        <a href="#"><img src={email} alt="Email"></a>
     </div>
+    <p>&copy; LASER 2023</p>
+    <p2>Numqam Solum Volare</p2>
 </div>
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,103 +1,120 @@
 <!-- Navbar.svelte -->
 <script>
-  import logo from '$lib/assets/Logo_V3.png';
-</script>
-
-<style>
-  /* Add your CSS styles here */
-  .navbar {
+    import logo from '$lib/assets/Logo_V3.png';
+    import '@fontsource-variable/exo-2';
+  </script>
+  
+  <style>
+    /* Add your CSS styles here */
+    .navbar {
       background: #111111; 
       display: flex;
       align-items: center;
-
       position: fixed;
       top: 0;
       width: 100%;
       z-index: 1000;
-
       padding: 1vh; 
       margin: 0; 
       border-bottom: 1px solid rgb(255, 255, 255);
-  }
-
-  .navbar-brand img {
+    }
+  
+    .logo-container {
+      display: flex;
+      align-items: center;
+    }
+  
+    .logo-container img {
       width: 4vh; 
       height: auto;
-  }
-
-  .navbar-links {
+    }
+  
+    .logo-text {
+      font-family: 'Exo 2 Variable', sans-serif;
+      font-size: 4vh;
+      color: white;
+      margin-left: 5px; 
+    }
+  
+    .navbar-links {
       flex: 1; 
       display: flex;
       justify-content: right; 
-  }
-
-  .navbar-links ul {
+    }
+  
+    .navbar-links ul {
       display: flex;
       list-style: none;
       margin: 0 2vw;
       padding: 0;
-  }
-
-  .navbar-links li:not(:last-child)::after {
+    }
+  
+    .navbar-links li:not(:last-child)::after {
       content: "";
       border-right: 1px solid white;
       height: 100%; 
       margin-left: 2vw;
       margin-right: 2vw;
       color: white;
-  }
-
-  .navbar-links a {
+    }
+  
+    .navbar-links a {
       color: white;
       text-decoration: none;
       font-size: 18px; 
       transition: color 0.3s ease;
-  }
-
-  .navbar-links a:hover {
+      font-family: 'Exo 2 Variable', sans-serif;
+    }
+  
+    .navbar-links a:hover {
       color: #58a6ff; 
-  }
-
-  @media (max-width: 768px) {
+    }
+  
+    @media (max-width: 768px) {
       .navbar {
-          flex-direction: column;
-          text-align: center;
+        flex-direction: column;
+        text-align: center;
       }
-
+  
       .navbar-links {
-          margin-top: 10px;
-          justify-content: center; 
+        margin-top: 10px;
+        justify-content: center; 
       }
-
+  
       .navbar-links li {
-          margin: 10px 0; 
+        margin: 10px 0; 
       }
+  
       .navbar-links li:not(:last-child)::after {
-      content: "";
-      border-right: 1px solid white;
-      height: 100%; 
-      margin-left: 4vw;
-      margin-right: 4vw;
-      color: white;
-  }
-  }
-</style>
-
-<div class="navbar">
-  <div class="navbar-brand">
-      <img src={logo} alt="LASER Logo">
-  </div>
-  <div class="navbar-links">
+        content: "";
+        border-right: 1px solid white;
+        height: 100%; 
+        margin-left: 4vw;
+        margin-right: 4vw;
+        color: white;
+      }
+    }
+  </style>
+  
+  <div class="navbar">
+    <div class="navbar-brand">
+      <div class="logo-container">
+        <img src={logo} alt="LASER Logo">
+        <span class="logo-text">LASER</span>
+      </div>
+    </div>
+    <div class="navbar-links">
       <ul>
-          <li>
-              <a class="nav-link" href="/" rel="prefetch">Home</a>
-          </li>
-          <li>
-              <a class="nav-link" href="settings" rel="prefetch">Projects</a>
-          </li>
-          <li>
-              <a class="nav-link" href="about" rel="prefetch">Contact</a>
-          </li>
+        <li>
+          <a class="nav-link" href="/" rel="prefetch">Home</a>
+        </li>
+        <li>
+          <a class="nav-link" href="settings" rel="prefetch">Projects</a>
+        </li>
+        <li>
+          <a class="nav-link" href="about" rel="prefetch">Contact</a>
+        </li>
       </ul>
+    </div>
   </div>
-</div>
+  
